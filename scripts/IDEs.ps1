bcbcb2f32cc272436ea407b1aa6353f3325c1870
Michael Åhs <turochamp@creatus.se>
2024-02-07 12:57:26 +0100

    Update IDEs.ps1

--- scripts/IDEs.ps1
+++ scripts/IDEs.ps1
@@ -3,7 +3,7 @@
 winget install -e -h --id Microsoft.VisualStudio.2022.Enterprise --silent --override "--wait --quiet --addProductLang En-us --config $env:USERPROFILE\dotfiles\config\vs2022\.vsconfig"
 # For simplicity, extensions will not be installed from here but manually selected from Roaming Extension Manager menu once logged in with personnalization account
 # Only exception is ozcode extension which requires and independent installer and therefore cannot be installed from VS.
-choco install -y ozcode-vs2022
+# choco install -y ozcode-vs2022
 
 # Chocolatey version is more up-o-date than winget version
 choco install -y vscode
